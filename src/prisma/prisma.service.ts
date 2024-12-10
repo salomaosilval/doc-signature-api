cf706b9d47d3e53ddcfc73e4164502b60f7ba200
salomaosilval <salomaosilvadev@gmail.com>
2024-12-10 14:05:40 -0400

    fix[prisma-service]: change method

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,9 +6,8 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
-
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
+    process.on('beforeExit', async () => {
       await app.close();
     });
   }
